85e92a4c9eb61381eca1fb98be9297afdbf88102
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-07 02:51:40 +0530

    [Akash]Added: Welcome message

---
+++ src/main/java/com/bridgelabz/restassuredemployeepayroll/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.restassuredemployeepayroll;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Rest API");
+	}
+}
